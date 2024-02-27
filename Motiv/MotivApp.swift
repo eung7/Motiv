8b7d8c4766c2786517690f74da88a495804ba2da
EungCheol Kim <wannabeeung@gmail.com>
2024-02-27 17:18:28 +0900

    🎉 First Commit

---
+++ Motiv/MotivApp.swift
@@ -0,0 +1,17 @@
+//
+//  MotivApp.swift
+//  Motiv
+//
+//  Created by 김응철 on 2/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MotivApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
